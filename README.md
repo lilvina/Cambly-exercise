dad88cdfd18a8c20348adaf3daa1bbce9f57b5b4
lilvina <davinaleo2007@yahoo.com>
2020-11-14 18:32:36 -0800

    finished take home project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Cambly-exercise
+
+Given a string of text, write a program that prints a list of unique words contained in the string along with the number
+of occurrences of each word in the string. The list should be sorted by the number of occurrences of each word in
+descending order.
+
+# Getting Started
+1. Download Atom
+2. Open terminal in Atom
+3. Run 'yarn install' to install modules
+4. Then run 'yarn start' to run it
---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require("file-system")
+
+fs.readFile('essay.txt', 'utf-8', (error, data) => {
+  if(error) {
+    throw error;
+  }
+
+  let newData = {}
+
+  let nextData = data.replace(/(\r\n|\n|\r)/gm, " ")
+
+  let nextData2 = nextData.replace(/[&\/\\#,+()$~%.'":;*?<>{}]/g, "")
+
+  let nextData3 = nextData2.toLowerCase()
+
+  let array = nextData3.split(' ')
+
+  for(let i = 0; i < array.length; i++) {
+    if(!(array[i] in newData)) {
+      newData[array[i]] = 1
+    } else {
+      newData[array[i]] += 1
+    }
+  }
+
+  let arrayOne = Object.keys(newData)
+  let arrayTwo = Object.values(newData)
+  let arrayMap = arrayOne.map((x, y) => {
+    return [x, arrayTwo[y]]
+  })
+  arrayMap.sort((a, b) => {
+    b[1] - a[1]
+  })
+  console.log(arrayMap)
+})
